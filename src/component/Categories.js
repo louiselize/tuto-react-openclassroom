27434c57df6f13ca95433bf6e8f7cdd9831c1039
Louise Lizé <louise.lize@icloud.com>
2025-01-24 18:51:37 +0100

    ✨ openclassroom tutor code update

--- src/component/Categories.js
+++ src/component/Categories.js
@@ -1,31 +1,23 @@
-import { plantList } from '../datas/plantList'
-import '../styles/ShoppingList.css'
+import '../styles/Categories.css'
 
-function Categories({ categoriesSelected, updateCategoriesSelected }) {
-    const categories = plantList.reduce(
-        (acc, plant) =>
-            acc.includes(plant.category) ? acc : acc.concat(plant.category),
-        []
-    )
-
-    return (
-        <div className="lmj-categories">
-            <select
-                value={categoriesSelected}
-                onChange={(e) => updateCategoriesSelected(e.target.value)}
-            >
-                <option value="">-- Toutes les catégories --</option>
-                {categories.map((cat) => (
-                    <option key={cat} value={cat}>
-                        {cat}
-                    </option>
-                ))}
-            </select>
-            <button onClick={() => updateCategoriesSelected('')}>
-                Réinitialiser
-            </button>
-        </div>
-    )
+function Categories({ setActiveCategory, categories, activeCategory }) {
+	return (
+		<div className='lmj-categories'>
+			<select
+				value={activeCategory}
+				onChange={(e) => setActiveCategory(e.target.value)}
+				className='lmj-categories-select'
+			>
+				<option value=''>---</option>
+				{categories.map((cat) => (
+					<option key={cat} value={cat}>
+						{cat}
+					</option>
+				))}
+			</select>
+			<button onClick={() => setActiveCategory('')}>Réinitialiser</button>
+		</div>
+	)
 }
 
 export default Categories
